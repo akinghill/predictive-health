fe235161dbc6a120df84e031404f1cd8bd76f993
Austin Hill <Austinkinghill@gmail.com>
2023-03-24 20:17:25 -0700

    upgrade insecure requests

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
